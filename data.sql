adcd8653abf02be584692bf2c32c5e23d3edfae4
aubinleyoung <aubinsimpeze01@gmail.com>
2023-03-29 13:25:09 +0300

    Delete values for species column

--- data.sql
+++ data.sql
@@ -8,10 +8,10 @@ VALUES
   (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) 
 VALUES 
-(5,'Charmander', '2020-02-08', 0, false, -11,'unspecified'),
-(6,'Plantmon', '2021-11-15', 2, true, -5.7,'unspecified'),
-(7,'Squirtle', '1993-04-02', 3, false, -12.13,'unspecified'),
-(8,'Angemon', '2005-06-12', 1, true, -45,'unspecified'),
-(9,'Boarmon', '2005-06-07', 7, true, 20.4,'unspecified'),
-(10,'Blossom', '1998-10-13', 3, true, 17,'unspecified'),
-(11,'Ditto', '2022-05-14', 4, true, 22,'unspecified');
+(5,'Charmander', '2020-02-08', 0, false, -11),
+(6,'Plantmon', '2021-11-15', 2, true, -5.7),
+(7,'Squirtle', '1993-04-02', 3, false, -12.13),
+(8,'Angemon', '2005-06-12', 1, true, -45),
+(9,'Boarmon', '2005-06-07', 7, true, 20.4),
+(10,'Blossom', '1998-10-13', 3, true, 17),
+(11,'Ditto', '2022-05-14', 4, true, 22);
